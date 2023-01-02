f5c43b1138b85dba2bfd83eff5fc407759f712e5
神楽坂雅詩 <cyscxchope@gmail.com>
2023-01-02 20:21:09 +0800

    创建plist测试文件生成用iOS工程

---
+++ plisttest/plisttest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  plisttest
+//
+//  Created by 神楽坂雅詩 on 2023/3/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ plisttest/plisttest/plisttestApp.swift
@@ -0,0 +1,17 @@
+//
+//  plisttestApp.swift
+//  plisttest
+//
+//  Created by 神楽坂雅詩 on 2023/3/9.
+//
+
+import SwiftUI
+
+@main
+struct plisttestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
