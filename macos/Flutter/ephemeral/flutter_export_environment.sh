9fe8f001ca767285a12a7739c4ccda7cbcaec15a
神楽坂雅詩 <cyscxchope@gmail.com>
2023-01-22 20:34:19 +0800

    修改macOS的窗口为手机比例以便调试

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Flutter_Windows"
-export "FLUTTER_APPLICATION_PATH=B:\settingspageflutter"
+export "FLUTTER_ROOT=/Volumes/MACAPP/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/MACAPP/c/SettingsPageFlutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
